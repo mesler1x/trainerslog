fdd300a724fae7bc1f373891026e8debc9ccbf6f
Roman Mesler <pay2mesler@gmail.com>
2024-03-07 09:56:26 +0500

    init

---
+++ src/main/java/ru/npcric/asparagus/trainerslog/TrainerslogApplication.java
@@ -0,0 +1,13 @@
+package ru.npcric.asparagus.trainerslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerslogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/npcric/asparagus/trainerslog/TrainerslogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.npcric.asparagus.trainerslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerslogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
