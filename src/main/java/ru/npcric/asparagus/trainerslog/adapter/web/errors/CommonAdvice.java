45dac8e0a3b1e14270ab731fdd647cd800d6a3f0
Roman Mesler <pay2mesler@gmail.com>
2024-03-16 17:15:17 +0500

    temp

--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/errors/CommonAdvice.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/errors/CommonAdvice.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
--- src/main/java/ru/npcric/asparagus/trainerslog/service/StudentService.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/service/StudentService.java
@@ -1,4 +1,7 @@
 package ru.npcric.asparagus.trainerslog.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
 }
