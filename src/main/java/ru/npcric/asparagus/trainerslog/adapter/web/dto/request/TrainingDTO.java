6f88fa73cc561a04730c3c3f893422a9ee89fd7c
Roman Mesler <meslerbeats@gmail.com>
2024-03-30 11:29:15 +0500

    add training + filial entities

--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/dto/request/TrainingDTO.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/dto/request/TrainingDTO.java
@@ -1,4 +1,7 @@
 package ru.npcric.asparagus.trainerslog.adapter.web.dto.request;
 
-public record TrainingDTO() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record TrainingDTO(LocalDateTime date, List<StudentDTO> studentDTOList) {
 }
--- src/main/java/ru/npcric/asparagus/trainerslog/domain/TrainingEntity.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/domain/TrainingEntity.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -21,9 +22,14 @@ public class TrainingEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-    LocalDate date;
+    LocalDateTime date;
     @ManyToMany
-    @JoinTable(name = "attendance",
+    @JoinTable(name = "TrainingsAndFilials",
+    joinColumns = @JoinColumn(name = "training_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "filial_id", referencedColumnName = "id"))
+    List<FilialEntity> filial;
+    @ManyToMany
+    @JoinTable(name = "attendance",// посещяемость
             joinColumns = @JoinColumn(name = "training_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "student_id", referencedColumnName = "id"))
     List<StudentEntity> studentEntityList;
