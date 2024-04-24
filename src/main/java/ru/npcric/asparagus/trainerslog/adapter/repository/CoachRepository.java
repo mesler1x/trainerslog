3cc612be0125f4fb2043b384d144da3d5b6d529a
Alexey Yurkov <yessirskill@yandex.ru>
2024-04-24 20:24:20 +0500

    Add coach delete

--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/repository/CoachRepository.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/repository/CoachRepository.java
@@ -1,6 +1,9 @@
 package ru.npcric.asparagus.trainerslog.adapter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.npcric.asparagus.trainerslog.domain.CoachEntity;
 import ru.npcric.asparagus.trainerslog.domain.user.UserEntity;
@@ -10,4 +13,10 @@ public interface CoachRepository extends JpaRepository<CoachEntity, Long> {
     CoachEntity findByUser(UserEntity user);
 
     CoachEntity findByUser_Username(String username);
+
+    @Modifying
+    @Query("UPDATE sgroup g SET g.coach = null WHERE g.coach.id = :trainerId")
+    void updateGroupsSetTrainerToNull(@Param("trainerId") Long trainerId);
+
+    void deleteById(Long id);
 }
