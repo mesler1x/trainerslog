af8b00b27a36bf64eda0dbecfec950691e5d43c6
Roman Mesler <pay2mesler@gmail.com>
2024-05-16 12:53:42 +0500

    fix CORS policy

--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/controller/FilialController.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/controller/FilialController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/controller/TrainingController.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/web/controller/TrainingController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.npcric.asparagus.trainerslog.service.TrainingService;
