ff9b6d5fcfb10d7637f8f9359e0098a5e3868325
Alexey Yurkov <yessirskill@yandex.ru>
2024-05-17 18:46:46 +0500

    add training scheduler

--- src/main/java/ru/npcric/asparagus/trainerslog/adapter/repository/TrainingRepository.java
+++ src/main/java/ru/npcric/asparagus/trainerslog/adapter/repository/TrainingRepository.java
@@ -1,12 +1,18 @@
 package ru.npcric.asparagus.trainerslog.adapter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.npcric.asparagus.trainerslog.domain.TrainingEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TrainingRepository extends JpaRepository<TrainingEntity, Long> {
     TrainingEntity findByDate(LocalDateTime dateTime);
+
+    @Query("SELECT t FROM TrainingEntity t WHERE t.date BETWEEN :startDate AND :endDate")
+    List<TrainingEntity> findTrainingsForThePastWeek(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
---
+++ src/main/java/ru/npcric/asparagus/trainerslog/service/scheduler/TrainingScheduler.java
@@ -0,0 +1,47 @@
+package ru.npcric.asparagus.trainerslog.service.scheduler;
+
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.npcric.asparagus.trainerslog.adapter.repository.TrainingRepository;
+import ru.npcric.asparagus.trainerslog.domain.TrainingEntity;
+import ru.npcric.asparagus.trainerslog.service.TrainingService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Log4j2
+@EnableAsync
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TrainingScheduler {
+    TrainingRepository trainingRepository;
+
+    @SchedulerLock(name = "duplicateLastWeekTrainings")
+    //Каждый понедельник в 00:01
+    @Scheduled(cron = "0 1 0 ? * MON", zone = "Asia/Yekaterinburg")
+    @SneakyThrows
+    @Transactional
+    public void autoCreateTrainings() {
+        log.info("Scheduler duplicateLastWeekTrainings start");
+
+        LocalDateTime nowTime = LocalDateTime.now();
+        List<TrainingEntity> trainingEntitiesOfLastWeek = trainingRepository.findTrainingsForThePastWeek(nowTime.minusWeeks(1), nowTime);
+        for(TrainingEntity trainingEntity : trainingEntitiesOfLastWeek) {
+            TrainingEntity newTraining = new TrainingEntity();
+            newTraining.setGroup(trainingEntity.getGroup());
+            newTraining.setDate(trainingEntity.getDate().plusWeeks(1));
+            trainingRepository.save(newTraining);
+        }
+
+        log.info("Scheduler duplicateLastWeekTrainings end");
+    }
+}
